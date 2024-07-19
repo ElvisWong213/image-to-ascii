d7d40adc5af58e6ead418f1c9abfcf877efb117e
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2024-07-19 03:34:11 +0100

    feat: add clap and support user input parameter

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
 use std::fs;
-
+use clap::Parser;
 use image::open;
 
 fn main() {
+    let args = Args::parse();
+
     let ascii_chars = ["." ,"," ,":" ,";" ,"+" ,"*" ,"?" ,"%" ,"$" ,"#" ,"@"];
-    let img = open("test.jpg").unwrap();
+    let img = match open(args.input_path) {
+        Ok(image) => image,
+        Err(error) => {
+            println!("{:}", error);
+            return;
+        },
+    };
     let luma_alpha = img.into_luma_alpha8();
-    let mut now_y = 0;
+    if luma_alpha.len() <= 0 {
+        println!("Unable to load image alpha channel data");
+        return;
+    }
+    let mut current_y = 0;
     let mut output = String::new();
 
     for data in luma_alpha.enumerate_pixels() {
-        // let x = data.0;
         let y = data.1;
         let lumaa = data.2;
         let alpha = lumaa.0[0] as f32 / lumaa.0[1] as f32;  
         let index: usize = (alpha * (ascii_chars.len() as f32 - 1.0)) as usize;
-        // println!("{:}, {:}, {:?}", x, y, alpha);
-        // print!("{:}", ascii_chars[index]);
         output.push_str(ascii_chars[index]);
-        if now_y != y {
-            // print!("\n");
+        if current_y != y {
             output.push_str("\n");
-            now_y = y;
+            current_y = y;
         }
     }
-    fs::write("test.txt", output).expect("Unable to write file");
+    fs::write(args.output_name + ".txt", output).expect("Unable to write file");
+}
+
+#[derive(Parser)]
+struct Args {
+    input_path: String,
+    output_name: String,
 }
