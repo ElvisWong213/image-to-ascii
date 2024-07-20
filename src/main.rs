262c92bd950d32ea995ca990ba2978ba1c2af336
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2024-07-20 16:56:47 +0100

    feat: add output width parameter

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
-use std::{fs, path::PathBuf};
 use clap::Parser;
-use image::open;
+use image::{open, DynamicImage};
+use std::{fs, path::PathBuf};
 
 fn main() {
     let args = Args::parse();
 
-    let ascii_chars = ["." ,"," ,":" ,";" ,"+" ,"*" ,"?" ,"%" ,"$" ,"#" ,"@"];
+    let ascii_chars = [".", ",", ":", ";", "+", "*", "?", "%", "$", "#", "@"];
+
+    // open image
     let img = match open(&args.input_path) {
-        Ok(image) => image,
+        Ok(image) => {
+            // resize image
+            let resized_image: DynamicImage = match args.width {
+                Some(new_width) => {
+                    let origin_width = image.width();
+                    let origin_height = image.height();
+                    let ratio = new_width as f32 / origin_width as f32;
+                    let new_height = (origin_height as f32 * ratio) as u32;
+                    image.resize(new_width, new_height, image::imageops::FilterType::Triangle)
+                }
+                None => image,
+            };
+            resized_image
+        }
         Err(error) => {
             println!("{:}", error);
             return;
-        },
+        }
     };
+
+    // get image alpha channel
     let luma_alpha = img.into_luma_alpha8();
     if luma_alpha.len() <= 0 {
         println!("Unable to load image alpha channel data");
         return;
     }
+
+    // convert image to ascii chars
     let mut current_y = 0;
     let mut output = String::new();
 
     for data in luma_alpha.enumerate_pixels() {
         let y = data.1;
         let lumaa = data.2;
-        let alpha = lumaa.0[0] as f32 / lumaa.0[1] as f32;  
+        let alpha = lumaa.0[0] as f32 / lumaa.0[1] as f32;
         let index: usize = (alpha * (ascii_chars.len() as f32 - 1.0)) as usize;
         output.push_str(ascii_chars[index]);
         if current_y != y {
@@ -32,7 +51,8 @@ fn main() {
             current_y = y;
         }
     }
-    
+
+    // save file
     let output_name = match args.output_name {
         Some(name) => name + ".txt",
         None => {
@@ -40,7 +60,12 @@ fn main() {
             if origin_input_path.set_extension("txt") == false {
                 panic!("Unable to change the wxtension");
             }
-            origin_input_path.file_name().unwrap().to_str().unwrap().to_string()
+            origin_input_path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
         }
     };
     fs::write(output_name, output).expect("Unable to write file");
@@ -52,4 +77,6 @@ struct Args {
     input_path: PathBuf,
     #[arg(short)]
     output_name: Option<String>,
+    #[arg(short)]
+    width: Option<u32>,
 }
